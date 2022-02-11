483f33d665a620baf423d0410029b9bd6f3422b3
CPTSven <94309344+CPTSven@users.noreply.github.com>
2022-02-11 03:20:32 +0300

    Покупка кириешек и их сметанизация
    Покрытие каждой кириешки сметанкой для лучшего вкуса и усвоения организмом

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
 import time
 import random
+from tqdm import tqdm
 
 a = ['думаю, ДА', "думаю, НЕТ"]
 print('У вас есть кириешки?')
 for i in range(3):
     time.sleep(1)
     print(i+1, 'sek')
-print(random.choice(a))
+kirieshka = random.choice(a)
+print(kirieshka)
 #print('Прости, я не могу предоставить тебе сметану')
+
+if kirieshka == "думаю, ДА":
+    time.sleep(2)
+    print('Дайте две пачки')
+    time.sleep(3)
+    print('С вас 35 рупис')
+    print('Начинаю процесс сметанизации кириешек')
+    for i in tqdm(range(int(9e6))):
+        pass
+    print('Процесс окончен')
+else:
+    time.sleep(2)
+    print('Жалко')
